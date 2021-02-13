f7377526909c6faa68351ac5e8b92e78a8819729
Wiltrouble <wiltrouble@gmail.com>
2021-02-13 15:37:32 -0400

    img added

--- js/Character.js
+++ js/Character.js
@@ -1,9 +1,10 @@
 export default class Character {
-    constructor([{name, nickname}]) {
+    constructor([{name, nickname, img}]) {
         this.name = name;
         this.nickname = nickname;
+        this.img = img;
         this.nameContainer = document.querySelector('#name');
-        
+        this.imageContainer = document.querySelector('#image')
         this.render();
 
         console.log(name)
@@ -16,8 +17,15 @@ export default class Character {
         `
     }
 
+    buildCharacterAvatar() {
+        return `
+            <img class="avatar" src=${this.img} width="350"></img>
+        `
+    }
+
     render() {
         this.nameContainer.innerHTML = this.buildCharacterData();
+        this.imageContainer.innerHTML = this.buildCharacterAvatar();
     }
 
 }
